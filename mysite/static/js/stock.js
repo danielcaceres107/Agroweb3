553a7575081c42225f7942ae87dcb1f9003ebec1
Daniel Caceres <dcaceres@servex-us.com>
2023-04-19 19:04:46 -0500

    first commit

---
+++ mysite/static/js/stock.js
@@ -0,0 +1,10 @@
+let stockProductos = [
+    {id: 1, nombre: "Papa", cantidad: 1, desc: "bulto 60-65kg aprox", precio: 85000, img: '../img/papa.jpg'},
+    {id: 2, nombre: "Yuca", cantidad: 1, desc: "bulto 65-70kg aprox", precio: 95000, img: '../img/yuca.jpg'},
+    {id: 3, nombre: "Tomate", cantidad: 1, desc: "canasta 50 unidades", precio: 115000, img: '../img/tomate.jpg'},
+    {id: 4, nombre: "Lechuga", cantidad: 1, desc: "canasta 24 unidades", precio: 30000, img: '../img/lechuga.jpg'},
+    {id: 5, nombre: "Zanahoria", cantidad: 1, desc: "bulto 45kg aprox", precio: 85000, img: '../img/zanahoria.jpg'},
+    {id: 6, nombre: "Cebolla", cantidad: 1, desc: "bulto 60-65kg aprox", precio: 155000, img: '../img/cebolla.jpg'},
+    {id: 7, nombre: "Aguacate", cantidad: 1, desc: "canasta 10 unidades", precio: 40000, img: '../img/aguacate.jpg' },
+    {id: 8, nombre: "Pepino", cantidad: 1, desc: "canasta 20 unidades", precio: 32000, img: '../img/pepino.jpg' }
+]
---
+++ mysite/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+def index(request):
+    username = 'Daniel Caceres'
+    return render(request, 'index.html', {'username' : username})
+
+def login(request):
+    return HttpResponse(render(request, 'login.html'))
